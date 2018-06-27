a3254ca403cbe938028d71029fe35948f3f782a1
papsiii <florian0stock@gmail.com>
2018-06-27 20:17:55 +0200

    Creation of an action bar and the ViewPager.

---
+++ app/src/main/java/com/mynews/flooo/mynews/AdapterPageActuality.java
@@ -0,0 +1,43 @@
+package com.mynews.flooo.mynews;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class AdapterPageActuality extends FragmentPagerAdapter
+{
+    private String[] tabTitles = new String[]{"Top Stories", "Most Popular", "Business"};
+    private int mNumOfTabs;
+
+    // 2 - Default Constructor
+    public AdapterPageActuality(FragmentManager mgr,int NumofTabs)
+    {
+        super(mgr);
+        this.mNumOfTabs = NumofTabs;
+        //this.colors = colors;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return (3);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        return tabTitles[position];
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        // 4 - Page to return
+        return (FragmentPageActuality.newInstance());
+    }
+
+
+
+
+}
--- app/src/main/java/com/mynews/flooo/mynews/MainActivity.java
+++ app/src/main/java/com/mynews/flooo/mynews/MainActivity.java
@@ -1,13 +1,58 @@
 package com.mynews.flooo.mynews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager pager = findViewById(R.id.viewpager);
+        pager.setAdapter(new AdapterPageActuality(getSupportFragmentManager(),tabLayout.getTabCount()));
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+
+            case R.id.action_add:
+                /* DO ADD */
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

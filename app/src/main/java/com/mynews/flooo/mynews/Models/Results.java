e55e1b3816997953498196e2c99b9c2cf04c5983
papsiii <florian0stock@gmail.com>
2018-10-04 11:23:15 +0200

    Update comments

--- app/src/main/java/com/mynews/flooo/mynews/Models/Results.java
+++ app/src/main/java/com/mynews/flooo/mynews/Models/Results.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Results extends ArrayList<News>
 {
 
+    // This is the main list who contains the news.
 
     private  int num_results;
     public void setNumResults(int num) {num_results=num;}
